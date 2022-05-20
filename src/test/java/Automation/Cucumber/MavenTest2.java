d444a94e8ac6df204ba68daa211ac733856f729b
alohaqa <aloha.openmethods2677@gmail.com>
2022-05-20 15:04:58 +0530

    This is my first commit...

---
+++ src/test/java/Automation/Cucumber/MavenTest2.java
@@ -0,0 +1,42 @@
+package Automation.Cucumber;
+
+import org.testng.annotations.Test;
+
+public class MavenTest2 {
+	
+	@Test
+	public void Test11() {
+		
+		System.out.println("Successfully Executed Sixth Test...!!!");
+	}
+	
+	@Test
+	public void Test12() {
+		
+		System.out.println("Successfully Executed Seventh Test...!!!");
+	}
+	
+	@Test
+	public void Test13() {
+		
+		System.out.println("Successfully Executed Eighth Test...!!!");
+	}
+	
+	@Test
+	public void Test14() {
+		
+		System.out.println("Successfully Executed Nineth Test...!!!");
+	}
+	
+	@Test
+	public void Test15() {
+		
+		System.out.println("Successfully Executed Tenth Test...!!!");
+	}
+	
+	
+	
+	
+	
+
+}
